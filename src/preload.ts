3b5b3046c78a18dfc4258549666775029f3461c5
goosewobbler <goosewobbler@pm.me>
2022-02-28 18:59:41 +0000

    preload API mappings

---
+++ src/preload.ts
@@ -0,0 +1,18 @@
+import { contextBridge, ipcRenderer } from 'electron';
+
+const validChannels = ['wdio-electron', 'wdio-electron.app', 'wdio-electron.mainProcess'];
+
+const invoke = (channel: string, ...data: unknown[]): Promise<unknown> =>
+  validChannels.includes(channel) ? ipcRenderer.invoke(channel, data) : Promise.reject();
+
+contextBridge.exposeInMainWorld('wdioElectron', {
+  custom: {
+    invoke: (...args: unknown[]) => invoke('wdio-electron', ...args),
+  },
+  mainProcess: {
+    invoke: (funcName: string, ...args: unknown[]) => invoke('wdio-electron.mainProcess', funcName, ...args),
+  },
+  app: {
+    invoke: (funcName: string, ...args: unknown[]) => invoke('wdio-electron.app', funcName, ...args),
+  },
+});
