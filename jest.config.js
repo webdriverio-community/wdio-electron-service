9c0ec84774ea292a25b193db8fcdcffed90d8c31
goosewobbler <goosewobbler@pm.me>
2022-04-08 10:57:57 +0100

    coverage => 50

--- jest.config.js
+++ jest.config.js
@@ -10,10 +10,10 @@ module.exports = {
   collectCoverageFrom: ['<rootDir>/src/*.ts'],
   coverageThreshold: {
     global: {
-      branches: 100,
-      functions: 100,
-      lines: 100,
-      statements: 100,
+      branches: 50,
+      functions: 50,
+      lines: 50,
+      statements: 50,
     },
   },
   clearMocks: true,
