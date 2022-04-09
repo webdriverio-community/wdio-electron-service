ded75aef9faf6563b9c9dc7f906df12ce393b9e5
goosewobbler <goosewobbler@pm.me>
2022-04-09 04:24:59 +0100

    increase coverage threshold

--- jest.config.js
+++ jest.config.js
@@ -10,10 +10,10 @@ module.exports = {
   collectCoverageFrom: ['<rootDir>/src/*.ts'],
   coverageThreshold: {
     global: {
-      branches: 50,
-      functions: 50,
-      lines: 50,
-      statements: 50,
+      branches: 85,
+      functions: 85,
+      lines: 85,
+      statements: 85,
     },
   },
   clearMocks: true,
