56d6f2680a7a46ca03f2100406e6d4e0c80037e0
goosewobbler <goosewobbler@protonmail.com>
2023-01-25 03:36:07 +0000

    improve interface

--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,9 @@ import ElectronWorkerService from './service.js';
 
 export default ElectronWorkerService;
 export const launcher = ChromedriverLauncher;
+export type electronService = {
+  api: (...arg: unknown[]) => Promise<unknown>;
+  app: (funcName: string, ...arg: unknown[]) => Promise<unknown>;
+  mainProcess: (funcName: string, ...arg: unknown[]) => Promise<unknown>;
+  browserWindow: (funcName: string, ...arg: unknown[]) => Promise<unknown>;
+} & WebdriverIO.Browser;
