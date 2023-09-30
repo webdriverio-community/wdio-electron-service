3d4072b5f7c7729ee147ca9ee30dccc765fdf6eb
Sam Maister <goosewobbler@protonmail.com>
2023-09-30 14:33:51 +0100

    use symlinks pt.1

--- example-cjs/src/main.ts
+++ example-cjs/src/main.ts
@@ -29,7 +29,7 @@ app.on('ready', () => {
     width: 200,
     height: 300,
     webPreferences: {
-      preload: `${appRootPath}/preload.js`,
+      preload: `${appRootPath}/preload.bundle.js`,
       nodeIntegration: false,
       contextIsolation: true,
     },
