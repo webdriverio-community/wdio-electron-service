a9134b01a7383d21b804999937bfd5f6d0b8972d
Sam Maister <goosewobbler@protonmail.com>
2023-09-30 14:35:22 +0100

    revert to import

--- example-cjs/wdio.conf.ts
+++ example-cjs/wdio.conf.ts
@@ -1,31 +1,7 @@
 import path from 'node:path';
 import fs from 'node:fs';
 
-// TODO: fix CJS import of utils
-// import { getBinaryPath } from 'wdio-electron-service/utils';
-
-function getBinaryPath(appPath: string, appName: string, distDirName = 'dist') {
-  const SupportedPlatform = {
-    darwin: 'darwin',
-    linux: 'linux',
-    win32: 'win32',
-  };
-  const { platform, arch } = process;
-
-  if (!Object.values(SupportedPlatform).includes(platform)) {
-    throw new Error(`Unsupported platform: ${platform}`);
-  }
-
-  const pathMap = {
-    darwin: path.join(arch === 'arm64' ? 'mac-arm64' : 'mac', `${appName}.app`, 'Contents', 'MacOS', appName),
-    linux: path.join('linux-unpacked', appName),
-    win32: path.join('win-unpacked', `${appName}.exe`),
-  };
-
-  const electronPath = pathMap[platform as keyof typeof SupportedPlatform];
-
-  return path.join(appPath, distDirName, electronPath);
-}
+import { getBinaryPath } from 'wdio-electron-service/utils';
 
 const packageJson = JSON.parse(fs.readFileSync('./package.json').toString());
 const {
