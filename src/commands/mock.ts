63d2e2acdbdc20ee18120527140922cce490f711
Sam Maister <goosewobbler@protonmail.com>
2023-11-08 15:30:46 +0000

    use @vitest/spy

--- src/commands/mock.ts
+++ src/commands/mock.ts
@@ -1,71 +1,82 @@
 import type * as Electron from 'electron';
 import log from '../log.js';
-import { fn, Mock } from '../thirdParty/spy.js'
+import { fn, type Mock } from '@vitest/spy';
 
-type Electron = typeof Electron
-type ElectronInterface = keyof Electron
+type ElectronType = typeof Electron;
+type ElectronInterface = keyof ElectronType;
 
-const getKeys = Object.keys as <T extends object>(obj: T) => Array<keyof T>
-const mockMap = new Map<string, Mock>()
+const getKeys = Object.keys as <T extends object>(obj: T) => Array<keyof T>;
+const mockMap = new Map<string, Mock>();
 
 export async function mock<Interface extends ElectronInterface>(
   apiName: Interface,
-  funcName: keyof Electron[Interface],
-  mockImplementation: (...args: any[]) => any | void = () => {}
+  funcName: keyof ElectronType[Interface],
+  mockImplementation: (...args: any[]) => any | void = () => {},
 ) {
-  const id = `${apiName}.${String(funcName)}`
-  await browser.electron.execute((electron, apiName, funcName) => {
-    electron[apiName][funcName] = fn(() => {}) as any
-  }, apiName, funcName)
+  const id = `${apiName}.${String(funcName)}`;
+  await browser.electron.execute(
+    (electron, apiName, funcName) => {
+      electron[apiName][funcName] = fn(() => {}) as any;
+    },
+    apiName,
+    funcName,
+  );
 
-  const mock = fn(mockImplementation)
-  mockMap.set(id, mock)
+  const mock = fn(mockImplementation);
+  mockMap.set(id, mock);
 
-  async function mockGetter () {
-    const mock = mockMap.get(id)
+  async function mockGetter() {
+    const mock = mockMap.get(id);
     if (!mock) {
-      throw new Error(`No mock registered for "${id}"`)
+      throw new Error(`No mock registered for "${id}"`);
     }
 
     const calls = await browser.electron.execute(
       (electron, apiName, funcName) => (electron[apiName][funcName] as Mock).mock.calls,
       apiName,
-      funcName
-    )
+      funcName,
+    );
 
     if (!calls) {
-      throw new Error(`mock for electron.${apiName}.${String(funcName)}() not found!`)
+      throw new Error(`mock for electron.${apiName}.${String(funcName)}() not found!`);
     }
 
     /**
      * create a fake mock to reapply calls on it
      */
     for (const call of calls) {
-      mock(...call)
+      mock(...call);
     }
 
-    return mock
+    return mock;
   }
 
-  patchMock(browser, mock, mockGetter, apiName, funcName)
-  return mockGetter
+  patchMock(browser, mock, mockGetter, apiName, funcName);
+  return mockGetter;
 }
 
-function patchMock<Interface extends ElectronInterface> (
+function patchMock<Interface extends ElectronInterface>(
   browser: WebdriverIO.Browser,
   mock: Mock,
   mockGetter: any,
   apiName: Interface,
-  funcName: keyof Electron[Interface]
+  funcName: keyof ElectronType[Interface],
 ) {
   log.debug(`patch mock instance for electron.${apiName}.${String(funcName)}(...)`);
   for (const fnName of getKeys(mock)) {
-    mockGetter[fnName] = (...args: unknown[]) => browser.electron.execute((electron, apiName, funcName, fnName, args) => {
-      if (typeof (electron[apiName][funcName] as Mock)[fnName] !== 'function') {
-        throw new Error(`mock for electron.${apiName}.${String(funcName)}() not found!`)
-      }
+    mockGetter[fnName] = (...args: unknown[]) =>
+      browser.electron.execute(
+        (electron, apiName, funcName, fnName, args) => {
+          if (typeof (electron[apiName][funcName] as Mock)[fnName] !== 'function') {
+            throw new Error(`mock for electron.${apiName}.${String(funcName)}() not found!`);
+          }
 
-      ;(electron[apiName][funcName] as any)[fnName](...args)
-    }, apiName, funcName, fnName, args) as any
+          (electron[apiName][funcName] as any)[fnName](...args);
+        },
+        apiName,
+        funcName,
+        fnName,
+        args,
+      ) as any;
   }
 }
