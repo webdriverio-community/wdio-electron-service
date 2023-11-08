b3ac35abc644bce3f11be38f74a18edb36988a6b
Sam Maister <goosewobbler@protonmail.com>
2023-11-08 14:32:53 +0000

    fix test

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
 import electron, { App, app, BrowserWindow, dialog, Dialog, ipcMain } from 'electron';
-import { fn } from './thirdParty/spy.js'
+import { fn } from './thirdParty/spy.js';
 
 import { Channel } from './constants.js';
 
@@ -7,7 +7,7 @@ import { Channel } from './constants.js';
  * allow `src/commands/mock.ts` to use the method
  */
 // @ts-ignore
-globalThis.fn = fn
+globalThis.fn = fn;
 
 type AppFunction = (this: App, ...args: unknown[]) => unknown;
 type MainProcessFunction = (this: NodeJS.Process, ...args: unknown[]) => unknown;
@@ -38,6 +38,10 @@ ipcMain.handle(Channel.Dialog, (_event, funcName: string, ...args: unknown[]) =>
   return dialogProp;
 });
 
+ipcMain.handle(Channel.Execute, (_, script: string, args: unknown[]) => {
+  return new Function(`return (${script}).apply(this, arguments)`)(electron, ...args);
+});
+
 ipcMain.handle(Channel.MainProcess, (_event, funcName: string, ...args: unknown[]) => {
   const processProp = process[funcName as keyof NodeJS.Process];
   if (typeof processProp === 'function') {
@@ -64,8 +68,4 @@ ipcMain.handle(Channel.Mock, (_event, apiName: string, funcName: string, mockRet
     funcName,
     mockReturnValue,
   };
-});
-
-ipcMain.handle(Channel.Execute, (_, script: string, args: unknown[]) => {
-  return new Function(`return (${script}).apply(this, arguments)`)(electron, ...args);
 });
--- test/main.spec.ts
+++ test/main.spec.ts
@@ -42,6 +42,7 @@ describe('main', () => {
       ['wdio-electron.app', expect.any(Function)],
       ['wdio-electron.browserWindow', expect.any(Function)],
       ['wdio-electron.dialog', expect.any(Function)],
+      ['wdio-electron.execute', expect.any(Function)],
       ['wdio-electron.mainProcess', expect.any(Function)],
       ['wdio-electron.mock', expect.any(Function)],
     ]);
