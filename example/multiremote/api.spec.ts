c2808f198b4bc4e6bf52fdd3bdf89f748c437b37
Sam Maister <goosewobbler@protonmail.com>
2023-11-30 17:34:25 +0000

    test: update for strict equality check

--- example/multiremote/api.spec.ts
+++ example/multiremote/api.spec.ts
@@ -12,17 +12,17 @@ const { name, version } = packageJson;
 describe('Electron APIs using Multiremote', () => {
   it('should retrieve app metadata through the electron API', async () => {
     const appName = await browser.electron.execute((electron) => electron.app.getName());
-    expect(appName).toEqual([name, name]);
+    expect(appName).toStrictEqual([name, name]);
     const appVersion = await browser.electron.execute((electron) => electron.app.getVersion());
-    expect(appVersion).toEqual([version, version]);
+    expect(appVersion).toStrictEqual([version, version]);
   });
 
   it('should allow to retrieve API values from single instance', async () => {
     const browserA = multiremotebrowser.getInstance('browserA');
-    expect(await browserA.electron.execute((electron) => electron.app.getName())).toBe([name, name]);
-    expect(await browserA.electron.execute((electron) => electron.app.getVersion())).toBe([version, version]);
+    expect(await browserA.electron.execute((electron) => electron.app.getName())).toStrictEqual([name, name]);
+    expect(await browserA.electron.execute((electron) => electron.app.getVersion())).toStrictEqual([version, version]);
     const browserB = multiremotebrowser.getInstance('browserB');
-    expect(await browserB.electron.execute((electron) => electron.app.getName())).toBe([name, name]);
-    expect(await browserB.electron.execute((electron) => electron.app.getVersion())).toBe([version, version]);
+    expect(await browserB.electron.execute((electron) => electron.app.getName())).toStrictEqual([name, name]);
+    expect(await browserB.electron.execute((electron) => electron.app.getVersion())).toStrictEqual([version, version]);
   });
 });
