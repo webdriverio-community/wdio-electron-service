6a60db4cea2626713b96d7ac636331ef91c24f0b
Sam Maister <goosewobbler@protonmail.com>
2023-12-10 21:55:36 +0000

    refactor: clean up types, extract for CJS

--- test/preload.spec.ts
+++ test/preload.spec.ts
@@ -1,7 +1,5 @@
 import { vi, describe, beforeEach, it, expect } from 'vitest';
 
-import type { WdioElectronWindowObj } from '../@types/wdio-electron-service';
-
 const ipcRendererInvokeMock = vi.fn();
 
 vi.mock('electron', () => ({
@@ -23,14 +21,14 @@ describe('preload', () => {
 
   it('should throw an error when the WDIO_ELECTRON environment variable does not exist', async () => {
     delete process.env.WDIO_ELECTRON;
-    await expect((window.wdioElectron as WdioElectronWindowObj).execute('look', ['some', 'args'])).rejects.toThrow(
+    await expect(window.wdioElectron.execute('look', ['some', 'args'])).rejects.toThrow(
       'Electron APIs can not be invoked outside of WDIO',
     );
     expect(ipcRendererInvokeMock).not.toHaveBeenCalled();
   });
 
   it('should call invoke with the expected params', async () => {
-    await (window.wdioElectron as WdioElectronWindowObj).execute('look', ['some', 'args']);
+    await window.wdioElectron.execute('look', ['some', 'args']);
     const ipcChannelName = 'wdio-electron.execute';
     expect(ipcRendererInvokeMock).toHaveBeenCalledWith(ipcChannelName, 'look', ['some', 'args']);
   });
