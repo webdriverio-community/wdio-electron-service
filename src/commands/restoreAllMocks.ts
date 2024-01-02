31be253bfdb75930e8d5aa54c37101f1bad4a672
Sam Maister <goosewobbler@protonmail.com>
2024-01-02 04:05:23 +0000

    refactor: streamline

--- src/commands/restoreAllMocks.ts
+++ src/commands/restoreAllMocks.ts
@@ -2,8 +2,7 @@ import mockStore from '../mockStore.js';
 import log from '../log.js';
 
 export async function restoreAllMocks(apiName?: string) {
-  for (const [mockName] of mockStore.getMocks()) {
-    const mock = await mockStore.getMock(mockName);
+  for (const [mockName, mock] of mockStore.getMocks()) {
     if (!apiName || mockName.match(new RegExp(`^electron.${apiName}`))) {
       log.debug('restoring mock', mockName);
       await mock.mockRestore();
