9b56fafbf78e687faa6a9746bf3dc34818743d64
Sam Maister <goosewobbler@protonmail.com>
2024-01-10 14:25:02 +0000

    test: add tests for `clearAllMocks` and `restoreAllMocks`

--- example-cjs/e2e/api.spec.ts
+++ example-cjs/e2e/api.spec.ts
@@ -65,6 +65,80 @@ describe('mockAll', () => {
   });
 });
 
+describe('clearAllMocks', () => {
+  it('should clear existing mocks', async () => {
+    const mockSetName = await browser.electron.mock('app', 'setName');
+    const mockWriteText = await browser.electron.mock('clipboard', 'writeText');
+
+    await browser.electron.execute((electron) => electron.app.setName('new app name'));
+    await browser.electron.execute((electron) => electron.clipboard.writeText('text to be written'));
+
+    await browser.electron.clearAllMocks();
+
+    expect(mockSetName.mock.calls).toStrictEqual([]);
+    expect(mockSetName.mock.instances).toStrictEqual([]);
+    expect(mockSetName.mock.invocationCallOrder).toStrictEqual([]);
+    expect(mockSetName.mock.lastCall).toBeUndefined();
+    expect(mockSetName.mock.results).toStrictEqual([]);
+
+    expect(mockWriteText.mock.calls).toStrictEqual([]);
+    expect(mockWriteText.mock.instances).toStrictEqual([]);
+    expect(mockWriteText.mock.invocationCallOrder).toStrictEqual([]);
+    expect(mockWriteText.mock.lastCall).toBeUndefined();
+    expect(mockWriteText.mock.results).toStrictEqual([]);
+  });
+
+  it('should clear existing mocks on an API', async () => {
+    const mockSetName = await browser.electron.mock('app', 'setName');
+    const mockWriteText = await browser.electron.mock('clipboard', 'writeText');
+
+    await browser.electron.execute((electron) => electron.app.setName('new app name'));
+    await browser.electron.execute((electron) => electron.clipboard.writeText('text to be written'));
+
+    await browser.electron.clearAllMocks('app');
+
+    expect(mockSetName.mock.calls).toStrictEqual([]);
+    expect(mockSetName.mock.instances).toStrictEqual([]);
+    expect(mockSetName.mock.invocationCallOrder).toStrictEqual([]);
+    expect(mockSetName.mock.lastCall).toBeUndefined();
+    expect(mockSetName.mock.results).toStrictEqual([]);
+
+    expect(mockWriteText.mock.calls).toStrictEqual([['text to be written']]);
+    expect(mockWriteText.mock.instances).toStrictEqual([expect.any(Function)]);
+    expect(mockWriteText.mock.invocationCallOrder).toStrictEqual([expect.any(Number)]);
+    expect(mockWriteText.mock.lastCall).toStrictEqual(['text to be written']);
+    expect(mockWriteText.mock.results).toStrictEqual([{ type: 'return', value: undefined }]);
+  });
+
+  it('should not reset existing mocks', async () => {
+    const mockGetName = await browser.electron.mock('app', 'getName');
+    const mockReadText = await browser.electron.mock('clipboard', 'readText');
+    await mockGetName.mockReturnValue('mocked appName');
+    await mockReadText.mockReturnValue('mocked clipboardText');
+
+    await browser.electron.clearAllMocks();
+
+    const appName = await browser.electron.execute((electron) => electron.app.getName());
+    const clipboardText = await browser.electron.execute((electron) => electron.clipboard.readText());
+    expect(appName).toBe('mocked appName');
+    expect(clipboardText).toBe('mocked clipboardText');
+  });
+
+  it('should not reset existing mocks on an API', async () => {
+    const mockGetName = await browser.electron.mock('app', 'getName');
+    const mockReadText = await browser.electron.mock('clipboard', 'readText');
+    await mockGetName.mockReturnValue('mocked appName');
+    await mockReadText.mockReturnValue('mocked clipboardText');
+
+    await browser.electron.clearAllMocks('app');
+
+    const appName = await browser.electron.execute((electron) => electron.app.getName());
+    const clipboardText = await browser.electron.execute((electron) => electron.clipboard.readText());
+    expect(appName).toBe('mocked appName');
+    expect(clipboardText).toBe('mocked clipboardText');
+  });
+});
+
 describe('resetAllMocks', () => {
   it('should clear existing mocks', async () => {
     const mockGetName = await browser.electron.mock('app', 'getName');
@@ -91,27 +165,25 @@ describe('resetAllMocks', () => {
   });
 
   it('should clear existing mocks on an API', async () => {
-    const mockGetName = await browser.electron.mock('app', 'getName');
-    const mockReadText = await browser.electron.mock('clipboard', 'readText');
-    await mockGetName.mockReturnValue('mocked appName');
-    await mockReadText.mockReturnValue('mocked clipboardText');
+    const mockSetName = await browser.electron.mock('app', 'setName');
+    const mockWriteText = await browser.electron.mock('clipboard', 'writeText');
 
-    await browser.electron.execute((electron) => electron.app.getName());
-    await browser.electron.execute((electron) => electron.clipboard.readText());
+    await browser.electron.execute((electron) => electron.app.setName('new app name'));
+    await browser.electron.execute((electron) => electron.clipboard.writeText('text to be written'));
 
     await browser.electron.resetAllMocks('app');
 
-    expect(mockGetName.mock.calls).toStrictEqual([]);
-    expect(mockGetName.mock.instances).toStrictEqual([]);
-    expect(mockGetName.mock.invocationCallOrder).toStrictEqual([]);
-    expect(mockGetName.mock.lastCall).toBeUndefined();
-    expect(mockGetName.mock.results).toStrictEqual([]);
+    expect(mockSetName.mock.calls).toStrictEqual([]);
+    expect(mockSetName.mock.instances).toStrictEqual([]);
+    expect(mockSetName.mock.invocationCallOrder).toStrictEqual([]);
+    expect(mockSetName.mock.lastCall).toBeUndefined();
+    expect(mockSetName.mock.results).toStrictEqual([]);
 
-    expect(mockReadText.mock.calls).toStrictEqual([[]]);
-    expect(mockReadText.mock.instances).toStrictEqual([expect.any(Function)]);
-    expect(mockReadText.mock.invocationCallOrder).toStrictEqual([expect.any(Number)]);
-    expect(mockReadText.mock.lastCall).toStrictEqual([]);
-    expect(mockReadText.mock.results).toStrictEqual([{ type: 'return', value: undefined }]);
+    expect(mockWriteText.mock.calls).toStrictEqual([['text to be written']]);
+    expect(mockWriteText.mock.instances).toStrictEqual([expect.any(Function)]);
+    expect(mockWriteText.mock.invocationCallOrder).toStrictEqual([expect.any(Number)]);
+    expect(mockWriteText.mock.lastCall).toStrictEqual(['text to be written']);
+    expect(mockWriteText.mock.results).toStrictEqual([{ type: 'return', value: undefined }]);
   });
 
   it('should reset existing mocks', async () => {
@@ -139,6 +211,43 @@ describe('resetAllMocks', () => {
     const appName = await browser.electron.execute((electron) => electron.app.getName());
     const clipboardText = await browser.electron.execute((electron) => electron.clipboard.readText());
     expect(appName).toBeUndefined();
+    expect(clipboardText).toBe('mocked clipboardText');
+  });
+});
+
+describe('restoreAllMocks', () => {
+  beforeEach(async () => {
+    await browser.electron.execute((electron) => {
+      electron.clipboard.clear();
+      electron.clipboard.writeText('some real clipboard text');
+    });
+  });
+
+  it('should restore existing mocks', async () => {
+    const mockGetName = await browser.electron.mock('app', 'getName');
+    const mockReadText = await browser.electron.mock('clipboard', 'readText');
+    await mockGetName.mockReturnValue('mocked appName');
+    await mockReadText.mockReturnValue('mocked clipboardText');
+
+    await browser.electron.restoreAllMocks();
+
+    const appName = await browser.electron.execute((electron) => electron.app.getName());
+    const clipboardText = await browser.electron.execute((electron) => electron.clipboard.readText());
+    expect(appName).toBe(pkgAppName);
+    expect(clipboardText).toBe('some real clipboard text');
+  });
+
+  it('should restore existing mocks on an API', async () => {
+    const mockGetName = await browser.electron.mock('app', 'getName');
+    const mockReadText = await browser.electron.mock('clipboard', 'readText');
+    await mockGetName.mockReturnValue('mocked appName');
+    await mockReadText.mockReturnValue('mocked clipboardText');
+
+    await browser.electron.restoreAllMocks('app');
+
+    const appName = await browser.electron.execute((electron) => electron.app.getName());
+    const clipboardText = await browser.electron.execute((electron) => electron.clipboard.readText());
+    expect(appName).toBe(pkgAppName);
     expect(clipboardText).toBe('mocked clipboardText');
   });
 });
