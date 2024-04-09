80c5b4faf5d80478b6611c75ab81aea0a7ea22cd
Sam Maister <goosewobbler@protonmail.com>
2024-04-09 18:50:06 +0100

    fix: more CJS issues

--- src/cjs/classes.ts
+++ src/cjs/classes.ts
@@ -5,30 +5,30 @@ import { Capabilities, Options, Services } from '@wdio/types';
 const dynamicImport = new Function('specifier', 'return import(specifier)');
 
 export class CJSElectronLauncher {
-  private instance?: Promise<Services.ServiceInstance>;
+  private instance: Promise<Services.ServiceInstance>;
 
   constructor(options: unknown, caps: unknown, config: Options.Testrunner) {
     this.instance = (async () => {
-      const importPath = '../service.js';
-      const { default: ElectronService } = await dynamicImport(importPath);
-      return new ElectronService(options, caps, config);
+      const importPath = '../launcher.js';
+      const { default: Launcher } = await dynamicImport(importPath);
+      return new Launcher(options, caps, config);
     })();
   }
 
   async onPrepare(config: Options.Testrunner, capabilities: Capabilities.RemoteCapabilities) {
-    const instance = await this.instance;
-    return instance?.onPrepare?.(config, capabilities);
+    const instance = (await this.instance) as Services.ServiceInstance;
+    return instance.onPrepare?.(config, capabilities);
   }
 }
 
 export class CJSElectronService {
-  private instance?: Promise<Services.ServiceInstance>;
+  private instance: Promise<Services.ServiceInstance>;
 
   constructor(globalOptions: unknown) {
     this.instance = (async () => {
       const importPath = '../service.js';
-      const { default: ElectronService } = await dynamicImport(importPath);
-      return new ElectronService(globalOptions);
+      const { default: Service } = await dynamicImport(importPath);
+      return new Service(globalOptions);
     })();
   }
 
@@ -38,12 +38,12 @@ export class CJSElectronService {
     specs: string[],
     cid: string,
   ) {
-    const instance = await this.instance;
-    return instance?.beforeSession?.(config, capabilities, specs, cid);
+    const instance = (await this.instance) as Services.ServiceInstance;
+    return instance.beforeSession?.(config, capabilities, specs, cid);
   }
 
   async before(capabilities: WebdriverIO.Capabilities, specs: string[], browser: WebdriverIO.Browser) {
-    const instance = await this.instance;
-    return instance?.before?.(capabilities, specs, browser);
+    const instance = (await this.instance) as Services.ServiceInstance;
+    return instance.before?.(capabilities, specs, browser);
   }
 }
