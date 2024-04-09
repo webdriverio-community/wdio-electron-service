c291e26b75d461b4517ae7f8decd67fd7f16b8ca
Sam Maister <goosewobbler@protonmail.com>
2024-04-09 13:33:33 +0100

    test: add platform switching for appBinaryPath

--- example/e2e-standalone/api.spec.ts
+++ example/e2e-standalone/api.spec.ts
@@ -3,7 +3,6 @@ import path from 'node:path';
 import fs from 'node:fs';
 
 import { startSession } from 'wdio-electron-service';
-import { expect } from '@wdio/globals';
 import type { PackageJson } from 'read-package-up';
 
 const __dirname = path.dirname(url.fileURLToPath(import.meta.url));
@@ -11,12 +10,22 @@ const packageJson = JSON.parse(
   fs.readFileSync(path.join(__dirname, '..', '..', 'package.json'), { encoding: 'utf-8' }),
 ) as PackageJson;
 
+const getBinaryExtension = () => {
+  if (process.platform === 'darwin') {
+    return '.app';
+  } else if (process.platform === 'win32') {
+    return '.exe';
+  }
+
+  return '';
+};
+
 const browser = await startSession({
-  appBinaryPath: './out/wdio-electron-service-example-darwin-arm64/wdio-electron-service-example.app',
+  appBinaryPath: `./out/wdio-electron-service-example-${process.platform}-${process.arch}/wdio-electron-service-example${getBinaryExtension()}`,
   appArgs: ['foo', 'bar=baz'],
 });
 
 const appName = await browser.electron.execute((electron) => electron.app.getName());
-expect(appName).toStrictEqual(packageJson.name);
-const appVersion = await browser.electron.execute((electron) => electron.app.getVersion());
-expect(appVersion).toStrictEqual(packageJson.version);
+if (appName !== packageJson.name) {
+  throw new Error(`appName test failed: ${appName} !== ${packageJson.name}`);
+}
