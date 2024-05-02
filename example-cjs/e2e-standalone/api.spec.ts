b3447b3e551657222fa32e05f600a528ca324277
goosewobbler <goosewobbler@protonmail.com>
2024-05-02 16:32:53 +0100

    refactor: use browser.deleteSession, remove CI guard

--- example-cjs/e2e-standalone/api.spec.ts
+++ example-cjs/e2e-standalone/api.spec.ts
@@ -3,7 +3,6 @@ import fs from 'node:fs';
 import process from 'node:process';
 
 import { startElectron } from 'wdio-electron-service';
-// import { isCI } from 'ci-info';
 import type { PackageJson } from 'read-package-up';
 
 process.env.TEST = 'true';
@@ -42,11 +41,8 @@ async function init() {
     throw new Error(`appVersion test failed: ${appVersion} !== ${packageJson.version}`);
   }
 
-  // Clean up - quit the app as it remains loaded
-  // Linux uses xvfb on CI, app.quit will break the E2Es in this case
-  // if (!isCI) {
+  // Clean up - quit the app
   await browser.deleteSession();
-  //}
 
   process.exit();
 }
