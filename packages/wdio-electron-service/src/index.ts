6001aee23e479fc16eddc52202845ca46447e9ea
goosewobbler <goosewobbler@protonmail.com>
2024-06-26 17:09:12 +0100

    refactor: align main package cjs with utils

--- packages/wdio-electron-service/src/index.ts
+++ packages/wdio-electron-service/src/index.ts
@@ -9,42 +9,6 @@ import ElectronWorkerService from './service.js';
 export const launcher = ElectronLaunchService;
 export default ElectronWorkerService;
 
-// export interface BrowserExtension {
-//   /**
-//    * Access the WebdriverIO Electron Service API.
-//    *
-//    * - {@link ElectronServiceAPI.clearAllMocks `browser.electron.clearAllMocks`} - Clear the Electron API mock functions
-//    * - {@link ElectronServiceAPI.execute `browser.electron.execute`} - Execute code in the Electron main process context
-//    * - {@link ElectronServiceAPI.mock `browser.electron.mock`} - Mock a function from the Electron API, e.g. `dialog.showOpenDialog`
-//    * - {@link ElectronServiceAPI.mockAll `browser.electron.mockAll`} - Mock an entire API object of the Electron API, e.g. `app` or `dialog`
-//    * - {@link ElectronServiceAPI.resetAllMocks `browser.electron.resetAllMocks`} - Reset the Electron API mock functions
-//    * - {@link ElectronServiceAPI.restoreAllMocks `browser.electron.restoreAllMocks`} - Restore the original Electron API functionality
-//    */
-//   electron: ElectronServiceAPI;
-// }
-
-// declare global {
-//   interface Window {
-//     wdioElectron: WdioElectronWindowObj;
-//   }
-
-//   namespace WebdriverIO {
-//     interface Browser extends BrowserExtension {}
-//     interface MultiRemoteBrowser extends BrowserExtension {}
-//     interface Capabilities {
-//       /**
-//        * custom capabilities to configure the Electron service
-//        */
-//       'wdio:electronServiceOptions'?: ElectronServiceOptions;
-//     }
-//   }
-
-//   var fn: typeof vitestFn;
-//   var originalApi: Record<ElectronInterface, ElectronType[ElectronInterface]>;
-//   var browser: WebdriverIO.Browser;
-//   var packageJson: PackageJson;
-// }
-
 export const browser: WebdriverIO.Browser = wdioBrowser;
 export const startElectron: (opts: ElectronServiceOptions) => Promise<WebdriverIO.Browser> = initSession;
 export * from '@repo/types';
