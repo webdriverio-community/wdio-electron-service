ab0a028c907ebb223a8243983004bc796c22590e
goosewobbler <goosewobbler@protonmail.com>
2024-06-26 17:20:22 +0100

    refactor: remove types export, standardise others

--- packages/wdio-electron-service/src/cjs/index.ts
+++ packages/wdio-electron-service/src/cjs/index.ts
@@ -5,9 +5,8 @@ import type { ElectronServiceOptions } from '@repo/types';
 import { init as initSession } from './session.js';
 import { CJSElectronLauncher, CJSElectronService } from './classes.js';
 
-exports.default = CJSElectronService;
-exports.launcher = CJSElectronLauncher;
+export default CJSElectronService;
+export const launcher = CJSElectronLauncher;
 
 export const browser: WebdriverIO.Browser = wdioBrowser;
 export const startElectron: (opts: ElectronServiceOptions) => Promise<WebdriverIO.Browser> = initSession;
-export * from '@repo/types';
