5d2a739ec7be99e96f6643c4d4482fc9c54975c8
goosewobbler <goosewobbler@protonmail.com>
2024-07-18 14:47:01 +0100

    fix: clean & scopes

---
+++ scripts/clean-forge-example.ts
@@ -0,0 +1,17 @@
+// This script is used to remove artifacts of the npm install for a forge example app
+import url from 'node:url';
+import path from 'node:path';
+
+import shell from 'shelljs';
+
+// read version from main package
+const __dirname = path.dirname(url.fileURLToPath(import.meta.url));
+
+// navigate to directory of target app
+shell.cd(path.join(__dirname, '..', 'apps', process.argv[2] || 'forge-esm'));
+
+// uninstall any dependencies installed with npm
+shell.exec('pnpm pkg delete dependencies');
+
+// install the correct workspace dependencies
+shell.exec('pnpm install wdio-electron-service@workspace:*');
