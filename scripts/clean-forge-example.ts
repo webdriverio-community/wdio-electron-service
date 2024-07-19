0cf9824b748bbc689ec9bf16f523ea1bc2231000
goosewobbler <goosewobbler@protonmail.com>
2024-07-19 17:20:49 +0100

    debug: try yarn

---
+++ scripts/clean-forge-example.ts
@@ -0,0 +1,17 @@
+// This script is used to remove artifacts of the npm install for a forge example app
+import url from 'node:url';
+import path from 'node:path';
+
+import shell from 'shelljs';
+
+// read version from main package
+const __dirname = path.dirname(url.fileURLToPath(import.meta.url));
+
+// navigate to directory of target app
+shell.cd(path.join(__dirname, '..', 'apps', process.argv[2] || 'forge-esm'));
+
+// remove any dependencies installed with yarn from the package.json
+shell.exec('pnpm pkg delete dependencies.wdio-electron-service dependencies.@repo/types dependencies.@repo/utils');
+
+// install the correct workspace dependencies
+shell.exec('pnpm pkg set dependencies.wdio-electron-service=workspace:*');
--- scripts/init-forge-example.ts
+++ scripts/init-forge-example.ts
@@ -1,4 +1,4 @@
-// Electron Forge does not support pnpm, so this script is used to initialise a forge example app with npm
+// Electron Forge does not support pnpm, so this script is used to initialise a forge example app with yarn
 import url from 'node:url';
 import path from 'node:path';
 import fs from 'node:fs';
@@ -14,15 +14,21 @@ const packageJson = JSON.parse(
   }),
 ) as PackageJson;
 
+// navigate to root directory
+shell.cd(path.join(__dirname, '..'));
+
+// delete corepack setting
+shell.exec('pnpm pkg delete packageManager');
+
 // navigate to directory of target app
 shell.cd(path.join(__dirname, '..', 'apps', process.argv[2] || 'forge-esm'));
 
 // remove any dependencies installed with pnpm
 shell.exec('pnpm clean');
 
-// install repo dependencies with npm
-shell.exec('npm install');
+// install repo dependencies with yarn
+shell.exec('yarn');
 shell.exec(
-  `npm install wdio-electron-service@file:../../packages/wdio-electron-service/wdio-electron-service-${packageJson.version}.tgz`,
+  `yarn add wdio-electron-service@file:../../packages/wdio-electron-service/wdio-electron-service-${packageJson.version}.tgz`,
 );
-shell.exec('npm install @repo/types@file:../../packages/types @repo/utils@file:../../packages/utils');
+shell.exec('yarn add @repo/types@file:../../packages/types @repo/utils@file:../../packages/utils');
