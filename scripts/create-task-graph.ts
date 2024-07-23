a72798a8ef0d812bbd88503d58c76686356f36ed
goosewobbler <goosewobbler@protonmail.com>
2024-07-23 13:15:01 +0100

    refactor: extract graphing

---
+++ scripts/create-task-graph.ts
@@ -0,0 +1,8 @@
+// Create a task graph for the specified tasks and open it in the default image viewer.
+// Usage: tsx scripts/create-task-graph.ts [graph-file] [tasks...]
+import shell from 'shelljs';
+
+const graphFile = process.argv[2] || 'task-graph.png';
+const tasks = process.argv.slice(3).join(' ');
+
+shell.exec(`turbo run ${tasks} --graph=${graphFile} && open ${graphFile}`);
