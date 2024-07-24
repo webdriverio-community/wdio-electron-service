25cbb9586ade331dbce427b0e66f355d0ae42308
goosewobbler <goosewobbler@protonmail.com>
2024-07-24 02:38:23 +0100

    test: add no-binary e2es

---
+++ packages/e2e/wdio.no-binary.standalone.ts
@@ -0,0 +1,11 @@
+import type { Options } from '@wdio/types';
+
+import { config as baseConfig } from './wdio.no-binary.conf.js';
+
+console.log('standalone config', baseConfig);
+
+export const config: Options.Testrunner = {
+  ...baseConfig,
+  outputDir: 'wdio-standalone-logs',
+  specs: ['./standalone/*.spec.ts'],
+};
