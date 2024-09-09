10614a0c12fe06669f54f75e511b4df1db977a92
goosewobbler <goosewobbler@protonmail.com>
2024-09-09 22:50:43 +0100

    chore: add comments

--- packages/wdio-electron-service/src/preload.ts
+++ packages/wdio-electron-service/src/preload.ts
@@ -3,8 +3,11 @@ import { Channel } from './constants.js';
 
 const invoke = async (channel: Channel, ...data: unknown[]) => ipcRenderer.invoke(channel, ...data);
 
+// Expose `execute` to the renderer process
 contextBridge.exposeInMainWorld('wdioElectron', {
   execute: (script: string, args: unknown[]) => invoke(Channel.Execute, script, args),
 });
 
+// Expose __name to the renderer process to work around issue with function serialization
+// https://github.com/privatenumber/tsx/issues/113
 contextBridge.exposeInMainWorld('__name', (func: (...args: unknown[]) => unknown) => func);
