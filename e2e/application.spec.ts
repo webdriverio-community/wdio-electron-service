ebdbda5c2a878c9e9eccc8c9fef2ceb5f01703bb
goosewobbler <goosewobbler@protonmail.com>
2024-11-04 17:25:39 +0000

    chore(E2E): standardise describes

--- e2e/application.spec.ts
+++ e2e/application.spec.ts
@@ -1,18 +1,16 @@
 import { expect } from '@wdio/globals';
 import { browser } from 'wdio-electron-service';
 
-describe('application loading', () => {
-  describe('App', () => {
-    it('should launch the application', async () => {
-      const title = await browser.getTitle();
-      expect(title).toEqual('Test');
-    });
+describe('application', () => {
+  it('should launch the application', async () => {
+    const title = await browser.getTitle();
+    expect(title).toEqual('Test');
+  });
 
-    it('should pass args through to the launched application', async () => {
-      // custom args are set in the wdio.conf.js file as they need to be set before WDIO starts
-      const argv = await browser.electron.execute(() => process.argv);
-      expect(argv).toContain('--foo');
-      expect(argv).toContain('--bar=baz');
-    });
+  it('should pass args through to the launched application', async () => {
+    // custom args are set in the wdio.conf.js file as they need to be set before WDIO starts
+    const argv = await browser.electron.execute(() => process.argv);
+    expect(argv).toContain('--foo');
+    expect(argv).toContain('--bar=baz');
   });
 });
