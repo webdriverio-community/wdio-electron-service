3bb80d2da2b651c26c32eac40f985c44bc39b362
goosewobbler <goosewobbler@protonmail.com>
2024-12-11 20:43:10 +0000

    ci: update release script

--- scripts/release.ts
+++ scripts/release.ts
@@ -1,13 +1,13 @@
-// Release script for the project
+// Release script for the project - publishes the packages to the npm registry
 // Usage: tsx scripts/release.ts [option1] [option2] [...]
 import shell from 'shelljs';
 
-const options = process.argv.slice(2).join(' ');
+const options = process.argv.slice(2);
 
 shell.cp(['README.md', 'LICENSE'], 'packages/wdio-electron-service');
 shell.cp(['LICENSE'], 'packages/@wdio_electron-utils');
 shell.cp(['LICENSE'], 'packages/@wdio_electron-types');
 
-shell.exec(`pnpm run release -- ${options}`);
-shell.exec('sleep 20');
-shell.exec(`release-it -VV ${options}`);
+const publishCommand = `pnpm publish -r ${options.join(' ')}`;
+
+shell.exec(publishCommand);
