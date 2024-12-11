3010d39d8184b11bc3d4d0eb0cd5c1119c3bb5dc
goosewobbler <goosewobbler@protonmail.com>
2024-12-11 19:00:52 +0000

    chore: fix paths

--- e2e/test/standalone/api.spec.ts
+++ e2e/test/standalone/api.spec.ts
@@ -12,7 +12,7 @@ process.env.TEST = 'true';
 
 const exampleDir = process.env.EXAMPLE_DIR || 'forge-esm';
 const __dirname = path.dirname(url.fileURLToPath(import.meta.url));
-const packageJsonPath = path.join(__dirname, '..', '..', 'apps', exampleDir, 'package.json');
+const packageJsonPath = path.join(__dirname, '..', '..', '..', 'apps', exampleDir, 'package.json');
 const packageJson = JSON.parse(fs.readFileSync(packageJsonPath, { encoding: 'utf-8' })) as NormalizedPackageJson;
 const pkg = { packageJson, path: packageJsonPath };
 const electronVersion = getElectronVersion(pkg);
