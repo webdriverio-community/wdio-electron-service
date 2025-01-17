b257e5bc63c344e7c705f88a7dd090787216d5d1
mato533 <35281743+mato533@users.noreply.github.com>
2025-01-18 03:59:37 +0900

    chore: update rollup.config.ts

--- packages/@wdio_electron-bundler/rollup.config.ts
+++ packages/@wdio_electron-bundler/rollup.config.ts
@@ -1,5 +1,49 @@
-import { RollupOptionCreator } from './src/index';
+import { nodeExternals, typescript, emitPackageJsonPlugin, readPackageJson, warnToErrorPlugin } from './src/index';
 
-const creator = new RollupOptionCreator();
+import type { RollupOptions } from 'rollup';
 
-export default creator.getConfigs();
+const pkgInfo = readPackageJson();
+
+const configEsm: RollupOptions = {
+  input: pkgInfo.input,
+  output: {
+    format: 'esm',
+    dir: pkgInfo.outDir.esm,
+    sourcemap: true,
+    plugins: [emitPackageJsonPlugin(pkgInfo.pkgName, 'esm')],
+  },
+  plugins: [
+    typescript({
+      compilerOptions: {
+        outDir: pkgInfo.outDir.esm,
+      },
+    }),
+    nodeExternals(),
+    warnToErrorPlugin(),
+  ],
+  strictDeprecations: true,
+};
+
+const configCjs: RollupOptions = {
+  input: pkgInfo.input,
+  output: {
+    format: 'cjs',
+    dir: pkgInfo.outDir.cjs,
+    exports: 'named',
+    dynamicImportInCjs: false,
+    sourcemap: true,
+    plugins: [emitPackageJsonPlugin(pkgInfo.pkgName, 'cjs')],
+  },
+  plugins: [
+    typescript({
+      compilerOptions: {
+        outDir: pkgInfo.outDir.cjs,
+      },
+    }),
+    nodeExternals(),
+    warnToErrorPlugin(),
+  ],
+  strictDeprecations: true,
+};
+
+export default [configEsm, configCjs];
--- packages/wdio-electron-service/rollup.config.ts
+++ packages/wdio-electron-service/rollup.config.ts
@@ -1,20 +1,68 @@
-import { RollupOptionCreator } from '@wdio/electron-bundler';
-import { nodeResolve } from '@rollup/plugin-node-resolve';
+import {
+  nodeExternals,
+  typescript,
+  emitPackageJsonPlugin,
+  readPackageJson,
+  warnToErrorPlugin,
+  injectDependencyPlugin,
+  nodeResolve,
+} from '@wdio/electron-bundler';
 
-const creator = new RollupOptionCreator({
-  options: {
-    cjs: {
-      externalOptions: {
-        exclude: 'fast-copy',
-      },
-    },
+import type { RollupOptions } from 'rollup';
+
+const pkgInfo = readPackageJson();
+
+const configEsm: RollupOptions = {
+  input: pkgInfo.input,
+  output: {
+    format: 'esm',
+    dir: pkgInfo.outDir.esm,
+    sourcemap: true,
+    plugins: [emitPackageJsonPlugin(pkgInfo.pkgName, 'esm')],
   },
-});
+  plugins: [
+    typescript({
+      compilerOptions: {
+        outDir: pkgInfo.outDir.esm,
+      },
+    }),
+    nodeExternals(),
+    nodeResolve(),
+    injectDependencyPlugin({
+      packageName: '@vitest/spy',
+      targetFile: 'index.js',
+      re: /export/,
+      importName: 'spy',
+      replace: (importName: string) => `const ${importName} =`,
+    }),
+    warnToErrorPlugin(),
+  ],
+  strictDeprecations: true,
+};
 
-const cjsConfig = creator.cjsRollupOptions;
+const configCjs: RollupOptions = {
+  input: pkgInfo.input,
+  output: {
+    format: 'cjs',
+    dir: pkgInfo.outDir.cjs,
+    exports: 'named',
+    dynamicImportInCjs: false,
+    sourcemap: true,
+    plugins: [emitPackageJsonPlugin(pkgInfo.pkgName, 'cjs')],
+  },
+  plugins: [
+    typescript({
+      compilerOptions: {
+        outDir: pkgInfo.outDir.cjs,
+      },
+    }),
+    nodeExternals({
+      exclude: 'fast-copy',
+    }),
+    nodeResolve(),
+    warnToErrorPlugin(),
+  ],
+  strictDeprecations: true,
+};
 
-if (typeof cjsConfig.plugins !== 'undefined' && Array.isArray(cjsConfig.plugins)) {
-  cjsConfig.plugins.push(nodeResolve());
-}
-
-export default creator.getConfigs();
+export default [configEsm, configCjs];
