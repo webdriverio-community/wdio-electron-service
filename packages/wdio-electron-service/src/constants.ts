2d81d239e9e8dcf6aa5d79917a6087cf2676daa5
mato533 <35281743+mato533@users.noreply.github.com>
2025-02-07 14:35:06 +0900

    chore: remove code related to IPC-Bridge

--- packages/wdio-electron-service/src/constants.ts
+++ packages/wdio-electron-service/src/constants.ts
@@ -3,7 +3,3 @@ export const APP_NOT_FOUND_ERROR =
   'If the application is not compiled, please do so before running your tests, e.g. via `%s`.\n' +
   'Otherwise if the application is compiled at a different location, please specify the `appBinaryPath` option in your capabilities.';
 export const CUSTOM_CAPABILITY_NAME = 'wdio:electronServiceOptions';
-
-export enum Channel {
-  Execute = 'wdio-electron.execute',
-}
