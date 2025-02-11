da338bc235ea9e1ee2cd4e69945259cee38b06f7
goosewobbler <432005+goosewobbler@users.noreply.github.com>
2025-02-11 16:13:34 +0000

    Merge pull request #898 from webdriverio-community/sm/rollup
    feat: improve the build using rollup
    (cherry picked from commit 48581bd26ef2dfa2c735360f8cad3cce1277b989)

---
+++ packages/@wdio_electron-bundler/src/index.ts
@@ -0,0 +1,34 @@
+import typescriptPlugin, { type RollupTypescriptOptions } from '@rollup/plugin-typescript';
+import { getInputConfig, getPackageJson, getOutDirs } from './utils';
+
+export { nodeExternals } from 'rollup-plugin-node-externals';
+export { nodeResolve } from '@rollup/plugin-node-resolve';
+export * from './plugins';
+
+export const typescript = (options: RollupTypescriptOptions = {}) => {
+  let exclude: string[] = [];
+
+  if (options.exclude) {
+    exclude = Array.isArray(options.exclude) ? options.exclude : [options.exclude];
+  }
+
+  return typescriptPlugin(
+    Object.assign({}, options, {
+      exclude: ['rollup.config.ts', ...exclude],
+      compilerOptions: Object.assign({}, options.compilerOptions, {
+        declaration: true,
+        declarationMap: true,
+      }),
+    }),
+  );
+};
+
+export const readPackageJson = (cwd = process.cwd()) => {
+  const pkg = getPackageJson(cwd);
+  const input = getInputConfig(pkg, 'src');
+  return {
+    input,
+    pkgName: pkg.packageJson.name,
+    outDir: getOutDirs(pkg),
+  };
+};
---
+++ packages/@wdio_electron-bundler/src/plugins.ts
@@ -0,0 +1,85 @@
+import { normalize } from 'node:path';
+import { injectDependency, type InjectDependencyPluginOptions } from './utils';
+import type { NormalizedPackageJson } from 'read-package-up';
+import type { Plugin } from 'rollup';
+
+export const MODULE_TYPE = {
+  CJS: 'cjs',
+  ESM: 'esm',
+} as const;
+
+type UnionizeValue<T> = T[keyof T];
+type SourceCodeType = UnionizeValue<typeof MODULE_TYPE>;
+
+const getTypeValue = (type: SourceCodeType) => {
+  switch (type) {
+    case MODULE_TYPE.CJS:
+      return 'commonjs';
+    case MODULE_TYPE.ESM:
+      return 'module';
+    default:
+      throw new Error(`Invalid type is specified: ${type}`);
+  }
+};
+
+export const getPackageJsonSource = (name: NormalizedPackageJson['name'], type: SourceCodeType) => ({
+  name: `${name}-${type}`,
+  type: getTypeValue(type),
+  private: true,
+});
+
+export const emitPackageJsonPlugin = (name: NormalizedPackageJson['name'], type: SourceCodeType): Plugin => {
+  const source = JSON.stringify(getPackageJsonSource(name, type), null, '  ');
+
+  return {
+    name: 'rollup-wdio-emit-package-json',
+    generateBundle(options) {
+      this.debug(`Emitting package.json for ${name}-${type} in ${options.dir}`);
+      this.emitFile({
+        type: 'asset',
+        fileName: 'package.json',
+        source: source,
+      });
+    },
+  };
+};
+
+export const warnToErrorPlugin = (): Plugin => {
+  return {
+    name: 'rollup-wdio-warn-to-error',
+    onLog(level, log) {
+      if (level === 'warn') {
+        this.warn(`Building Rollup produced warnings that need to be resolved.`);
+        this.error(log);
+      }
+    },
+  };
+};
+
+export const injectDependencyPlugin = (
+  options: InjectDependencyPluginOptions | InjectDependencyPluginOptions[],
+): Plugin => {
+  const pluginOptions = Array.isArray(options) ? options : [options];
+  const targetFiles = [...new Set(pluginOptions.map((item) => item.targetFile))];
+  const determineTarget = (id: string, target: string) => normalize(id).endsWith(normalize(target));
+
+  return {
+    name: 'rollup-wdio-inject-dependency',
+    async transform(code, id) {
+      const targetFile = targetFiles.find((targetFile) => determineTarget(id, targetFile));
+      if (!targetFile) {
+        return null;
+      }
+
+      const targetOptions = pluginOptions.filter((pluginOption) => pluginOption.targetFile === targetFile);
+
+      let newCode = code;
+      for (const targetOption of targetOptions) {
+        newCode = await injectDependency.call(this, targetOption, newCode);
+      }
+      return { code: newCode, map: null };
+    },
+  };
+};
+
+export { type InjectDependencyPluginOptions };
