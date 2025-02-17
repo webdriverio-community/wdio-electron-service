d9208856016e1be48e597f3c3d5501151ebd2617
mato533 <35281743+mato533@users.noreply.github.com>
2025-02-17 17:18:39 +0900

    feat: add cdp-bridge for electron

--- packages/wdio-electron-service/src/bridge.ts
+++ packages/wdio-electron-service/src/bridge.ts
@@ -1,4 +1,6 @@
+import os from 'node:os';
 import log from '@wdio/electron-utils/log';
+import { CdpBridge } from '@wdio/cdp-bridge';
 import { SevereServiceError } from 'webdriverio';
 
 export const getDebuggerEndpoint = (capabilities: WebdriverIO.Capabilities) => {
@@ -18,3 +20,59 @@ export const getDebuggerEndpoint = (capabilities: WebdriverIO.Capabilities) => {
   log.trace(`Detected the node debugger endpoint: `, result);
   return result;
 };
+
+export class ElectronCdpBridge extends CdpBridge {
+  #contextId: number = 0;
+
+  get contextId() {
+    return this.#contextId;
+  }
+
+  async connect(): Promise<void> {
+    await super.connect();
+
+    const contextHandler = this.#getContextIdHandler();
+
+    await this.send('Runtime.enable');
+    await this.send('Runtime.disable');
+
+    this.#contextId = await contextHandler;
+
+    const scripts = [
+      // Add __name to the global object to work around issue with function serialization
+      // This enables browser.execute to work with scripts which declare functions (affects TS specs only)
+      // https://github.com/webdriverio-community/wdio-electron-service/issues/756
+      // https://github.com/privatenumber/tsx/issues/113
+      `globalThis.__name = globalThis.__name ?? ((func) => func);`,
+      // Add electron to the global object
+      `globalThis.electron = require('electron');`,
+    ];
+    // add because windows
+    if (os.type().match('Windows')) {
+      scripts.push(`globalThis.process = require('node:process');`);
+    }
+
+    await this.send('Runtime.evaluate', {
+      expression: scripts.join('\n'),
+      includeCommandLineAPI: true,
+      replMode: true,
+      contextId: this.#contextId,
+    });
+  }
+
+  #getContextIdHandler() {
+    return new Promise<number>((resolve, reject) => {
+      this.on('Runtime.executionContextCreated', (params) => {
+        if (params.context.auxData.isDefault) {
+          resolve(params.context.id);
+        }
+      });
+
+      setTimeout(() => {
+        const err = new Error('Timeout exceeded to get the ContextId.');
+        log.error(err.message);
+        reject(err);
+      }, this.options.timeout);
+    });
+  }
+}
--- packages/wdio-electron-service/test/bridge.spec.ts
+++ packages/wdio-electron-service/test/bridge.spec.ts
@@ -1,6 +1,18 @@
-import { describe, expect, it } from 'vitest';
+import os from 'node:os';
+import { beforeEach, describe, expect, it, vi } from 'vitest';
 
-import { getDebuggerEndpoint } from '../src/bridge.js';
+import { ElectronCdpBridge, getDebuggerEndpoint } from '../src/bridge.js';
+import { CdpBridge } from '@wdio/cdp-bridge';
+
+vi.mock('@wdio/electron-utils/log', () => ({
+  default: {
+    info: vi.fn(),
+    warn: vi.fn(),
+    debug: vi.fn(),
+    error: vi.fn(),
+    trace: vi.fn(),
+  },
+}));
 
 describe('getDebuggerEndpoint', () => {
   it('should return the endpoint information of the node debugger', () => {
@@ -49,5 +61,103 @@ describe('getDebuggerEndpoint', () => {
         },
       }),
     ).toThrowError();
+  });
+});
+
+describe('ElectronCdpBridge', () => {
+  vi.mock('@wdio/cdp-bridge', async (importOriginal) => {
+    const actual = await importOriginal<typeof import('@wdio/cdp-bridge')>();
+    actual.CdpBridge.prototype.connect = vi.fn();
+    actual.CdpBridge.prototype.send = vi.fn();
+    actual.CdpBridge.prototype.on = vi.fn();
+    return {
+      CdpBridge: actual.CdpBridge,
+    };
+  });
+
+  beforeEach(() => {
+    vi.restoreAllMocks();
+  });
+  describe('connect', async () => {
+    const expectedContextId = 999;
+    const getMockedInstance = async () => {
+      const cdpBridge = new ElectronCdpBridge();
+
+      const promise = cdpBridge.connect();
+      const mockedInstance = (await vi
+        .mocked(CdpBridge.prototype.connect)
+        .mock.instances.slice(-1)[0]) as unknown as ElectronCdpBridge;
+
+      const [_method, callback] = vi.mocked(mockedInstance.on).mock.calls.slice(-1)[0];
+      callback(
+        {
+          context: {
+            id: expectedContextId,
+            auxData: {
+              isDefault: true,
+            },
+          },
+        } as any, // eslint-disable-line @typescript-eslint/no-explicit-any
+      );
+      await promise;
+      return mockedInstance;
+    };
+
+    it('should throw error when getting contextId with timeout', async () => {
+      const cdpBridge = new ElectronCdpBridge({ timeout: 10 });
+      await expect(() => cdpBridge.connect()).rejects.toThrowError('Timeout exceeded to get the ContextId.');
+    });
+
+    it('should call super.on() with expected arguments', async () => {
+      const mockedInstance = await getMockedInstance();
+      const [method, _callback] = vi.mocked(mockedInstance.on).mock.calls.slice(-1)[0];
+      expect(mockedInstance.on).toHaveBeenCalledTimes(1);
+      expect(method).toBe('Runtime.executionContextCreated');
+    });
+
+    it('should contextId is set', async () => {
+      const mockedInstance = await getMockedInstance();
+      expect((mockedInstance as unknown as ElectronCdpBridge).on).toHaveBeenCalledTimes(1);
+      expect(mockedInstance.contextId).toBe(expectedContextId);
+    });
+
+    it('should call super.on() with expected', async () => {
+      vi.spyOn(os, 'type').mockReturnValue('Windows');
+      const mockedInstance = await getMockedInstance();
+      const expectedArgsOfEval = {
+        contextId: expectedContextId,
+        expression: [
+          'globalThis.__name = globalThis.__name ?? ((func) => func);',
+          "globalThis.electron = require('electron');",
+          "globalThis.process = require('node:process');",
+        ].join('\n'),
+        includeCommandLineAPI: true,
+        replMode: true,
+      };
+
+      expect(mockedInstance.send).toHaveBeenCalledTimes(3);
+      expect(mockedInstance.send).toHaveBeenNthCalledWith(1, 'Runtime.enable');
+      expect(mockedInstance.send).toHaveBeenNthCalledWith(2, 'Runtime.disable');
+      expect(mockedInstance.send).toHaveBeenNthCalledWith(3, 'Runtime.evaluate', expectedArgsOfEval);
+    });
+
+    it('should call super.on() with expected on not windows', async () => {
+      vi.spyOn(os, 'type').mockReturnValue('Linux');
+      const mockedInstance = await getMockedInstance();
+      const expectedArgsOfEval = {
+        contextId: expectedContextId,
+        expression: [
+          'globalThis.__name = globalThis.__name ?? ((func) => func);',
+          "globalThis.electron = require('electron');",
+        ].join('\n'),
+        includeCommandLineAPI: true,
+        replMode: true,
+      };
+
+      expect(mockedInstance.send).toHaveBeenCalledTimes(3);
+      expect(mockedInstance.send).toHaveBeenNthCalledWith(1, 'Runtime.enable');
+      expect(mockedInstance.send).toHaveBeenNthCalledWith(2, 'Runtime.disable');
+      expect(mockedInstance.send).toHaveBeenNthCalledWith(3, 'Runtime.evaluate', expectedArgsOfEval);
+    });
   });
 });
