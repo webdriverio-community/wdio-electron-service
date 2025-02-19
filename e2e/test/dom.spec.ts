7c7ee4e525df1e165af63ddfd014478a72b95f59
mato533 <35281743+mato533@users.noreply.github.com>
2025-02-19 12:42:49 +0900

    test: add test for issue

--- e2e/test/dom.spec.ts
+++ e2e/test/dom.spec.ts
@@ -6,6 +6,13 @@ describe('DOM', () => {
   let screen: WebdriverIOQueries;
 
   before(() => {
+    /**
+     * This is a workaround for the issue with the `browser` object type being
+     * mismatched`.
+     * @see https://github.com/testing-library/webdriverio-testing-library/issues/51
+     */
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-expect-error
     screen = setupBrowser(browser);
   });
 
@@ -28,8 +35,16 @@ describe('DOM', () => {
   });
 
   describe('using $', () => {
+    /**
+     * This test is to check if return value of `$` has `getAttribute` or another attributes
+     * @see https://github.com/webdriverio-community/wdio-electron-service/issues/957
+     */
     it('should determine when an element is in the document', async () => {
-      await expect($('[data-testid="disabled-checkbox"]')).toExist();
+      const checkbox = $('[data-testid="disabled-checkbox"]');
+      const type = await checkbox.getAttribute('type');
+
+      await expect(checkbox).toExist();
+      await expect(type).toBe('checkbox');
     });
 
     it('should determine when an element is not in the document', async () => {
@@ -47,8 +62,12 @@ describe('DOM', () => {
 });
 
 describe('using $$', () => {
+  /**
+   * This test is to check if return value of `$$` has `getElements` or another attributes
+   * @see https://github.com/webdriverio-community/wdio-electron-service/issues/899
+   */
   it('should be able to call getElements to return values', async () => {
-    const result = await $$('[data-testid="disabled-checkbox"]');
+    const result = $$('[data-testid="disabled-checkbox"]');
     const chainedResult = await result.getElements();
     expect(chainedResult.length).toBe(1);
   });
