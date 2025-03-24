ef626778247c6f7d0d0323aab3a2e34de33f5cc5
Sam Maister <goosewobbler@protonmail.com>
2025-03-24 00:52:27 +0000

    test: add multiremote type

--- e2e/test/window/window.multiremote.spec.ts
+++ e2e/test/window/window.multiremote.spec.ts
@@ -2,15 +2,19 @@ import { multiremotebrowser, expect } from '@wdio/globals';
 
 describe('application with changing window', () => {
   it('should launch the application splash screen window', async () => {
-    const browserA = multiremotebrowser.getInstance('browserA');
-    const browserB = multiremotebrowser.getInstance('browserB');
+    const multi = multiremotebrowser as WebdriverIO.MultiRemoteBrowser;
+
+    const browserA = multi.getInstance('browserA');
+    const browserB = multi.getInstance('browserB');
     await expect(browserA).toHaveTitle('Splash Screen');
     await expect(browserB).toHaveTitle('Splash Screen');
   });
 
   it('should switch to the application main window', async () => {
-    const browserA = multiremotebrowser.getInstance('browserA');
-    const browserB = multiremotebrowser.getInstance('browserB');
+    const multi = multiremotebrowser as WebdriverIO.MultiRemoteBrowser;
+
+    const browserA = multi.getInstance('browserA');
+    const browserB = multi.getInstance('browserB');
     await (await browserA.$('.switch-main-window')).click();
     await (await browserB.$('.switch-main-window')).click();
     await expect(browserA).toHaveTitle('Test');
