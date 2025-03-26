051f31899f6ffd9b77121ca85d9d5f1d2e58a154
mato533 <taiki.abe.00@gmail.com>
2025-03-27 00:15:50 +0900

    refactor: split type definition for utils

---
+++ packages/@wdio_electron-utils/src/types.ts
@@ -0,0 +1,27 @@
+import type { AppBuildInfo, BuilderBuildInfo, ForgeBuildInfo } from '@wdio/electron-types';
+import type { SUPPORTED_PLATFORM } from './constants';
+
+export type SupportedPlatform = keyof typeof SUPPORTED_PLATFORM;
+
+export interface IBinaryPathGenerator {
+  generate(): string[];
+}
+export interface IExecutablePath {
+  binary: IBinaryPathGenerator;
+  get(): Promise<string>;
+}
+
+export type CommonBinaryOptions = {
+  appBuildInfo: AppBuildInfo;
+  platform: SupportedPlatform;
+  electronVersion?: string;
+  packageJsonPath: string;
+};
+
+export type ForgeBinaryOptions = Omit<CommonBinaryOptions, 'appBuildInfo'> & {
+  appBuildInfo: ForgeBuildInfo;
+};
+
+export type BuilderBinaryOptions = Omit<CommonBinaryOptions, 'appBuildInfo'> & {
+  appBuildInfo: BuilderBuildInfo;
+};
