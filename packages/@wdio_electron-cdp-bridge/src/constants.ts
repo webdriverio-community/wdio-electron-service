3b4214302cd4ee32379abe27088a15d49e199e22
Sam Maister <goosewobbler@protonmail.com>
2025-04-03 18:21:14 +0100

    refactor: improve verbiage

--- packages/@wdio_electron-cdp-bridge/src/constants.ts
+++ packages/@wdio_electron-cdp-bridge/src/constants.ts
@@ -5,12 +5,12 @@ export const DEFAULT_MAX_RETRY_COUNT = 3;
 export const DEFAULT_RETRY_INTERVAL = 100;
 
 export const ERROR_MESSAGE = {
-  TIMEOUT_CONNECTION: 'Timeout exceeded when wait for the response:',
-  TIMEOUT_WAIT_PORT: 'Timeout exceeded to wait port opening',
-  DEBUGGER_NOT_FOUND: 'No debugger was detected.',
-  DEBUGGER_FOUND_MULTIPLE: 'Multiple debugger was detected. Use first debugger.',
-  NOT_CONNECTED: "WebSocket is not connected. Call 'DebuggerClient.connect()' before call this method.'",
-  CONNECTION_CLOSED: 'Connection has been closed.',
-  ERROR_PARSE_JSON: 'Failed to parse JSON:',
-  ERROR_INTERNAL: 'Close the connection due to error:',
+  TIMEOUT_CONNECTION: 'Request timeout exceeded waiting for response:',
+  TIMEOUT_WAIT_PORT: 'Timeout exceeded while waiting for debugger port to open',
+  DEBUGGER_NOT_FOUND: 'No debugger instance was detected',
+  DEBUGGER_FOUND_MULTIPLE: 'Multiple debugger instances detected. Using the first available instance',
+  NOT_CONNECTED: "WebSocket is not connected. Call 'CdpBridge.connect()' before using this method",
+  CONNECTION_CLOSED: 'WebSocket connection has been closed',
+  ERROR_PARSE_JSON: 'Failed to parse JSON response:',
+  ERROR_INTERNAL: 'Connection closed due to error:',
 } as const;
