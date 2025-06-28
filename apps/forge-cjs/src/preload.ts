345f5194a0ea00ea8369e74a6c9d85c630826649
Sam Maister <goosewobbler@protonmail.com>
2025-06-28 15:49:31 +0100

    chore: reinstate apps dir

---
+++ apps/forge-cjs/src/preload.ts
@@ -0,0 +1,10 @@
+import { contextBridge, ipcRenderer } from 'electron';
+
+const validChannels = ['increase-window-size', 'decrease-window-size', 'show-open-dialog', 'switch-main-window'];
+
+const invoke = (channel: string, ...data: unknown[]) =>
+  validChannels.includes(channel) ? ipcRenderer.invoke(channel, data) : Promise.reject();
+
+contextBridge.exposeInMainWorld('api', {
+  invoke,
+});
