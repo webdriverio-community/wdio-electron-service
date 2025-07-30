5c9861ecbc9d453c3eccb10ad3efc4e8cd63bfc7
Sam Maister <goosewobbler@protonmail.com>
2025-07-30 11:25:38 +0100

    fix(package-tests): apply workaround

--- fixtures/package-tests/script-app/electron.vite.config.ts
+++ fixtures/package-tests/script-app/electron.vite.config.ts
@@ -18,5 +18,11 @@ export default defineConfig({
       },
     },
   },
-  renderer: {},
+  renderer: {
+    resolve: {
+      // workaround for windows path issue
+      // see https://github.com/alex8088/electron-vite/issues/802
+      preserveSymlinks: true,
+    },
+  },
 });
