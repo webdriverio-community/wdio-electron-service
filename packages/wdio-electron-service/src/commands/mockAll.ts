b941e58a2d13ac0aae628c10e2dc8a9659ad3fb7
Sam Maister <goosewobbler@protonmail.com>
2025-08-06 15:17:07 +0100

    fix(electron-service): clean up mockAll object spread with reduce

--- packages/wdio-electron-service/src/commands/mockAll.ts
+++ packages/wdio-electron-service/src/commands/mockAll.ts
@@ -2,16 +2,18 @@ import type { ElectronMock, ExecuteOpts } from '@wdio/electron-types';
 import { mock } from './mock.js';
 
 export async function mockAll(apiName: string) {
-  const apiFnNames = await browser.electron.execute<string, [string, ExecuteOpts]>(
-    (electron, apiName) => Object.keys(electron[apiName as keyof typeof electron]).toString(),
-    apiName,
-    { internal: true },
-  );
+  const apiFnNames = (
+    await browser.electron.execute<string, [string, ExecuteOpts]>(
+      (electron, apiName) => Object.keys(electron[apiName as keyof typeof electron]).toString(),
+      apiName,
+      { internal: true },
+    )
+  ).split(',');
   const mockedApis: Record<string, ElectronMock> = {};
-  const funcNames = apiFnNames.split(',');
-  for (const funcName of funcNames) {
-    mockedApis[funcName] = () => undefined;
-  }
+
+  apiFnNames.forEach((funcName) => {
+    mockedApis[funcName] = (() => undefined) as unknown as ElectronMock;
+  });
 
   for (const funcName in mockedApis) {
     mockedApis[funcName] = await mock(apiName, funcName);
