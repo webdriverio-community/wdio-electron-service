ade8cb5e012126aa62f01e63941d40c9413f772b
Sam Maister <goosewobbler@protonmail.com>
2025-08-08 16:32:43 +0100

    test(utils): update logger mock

--- packages/@wdio_electron-utils/test/__mock__/log.ts
+++ packages/@wdio_electron-utils/test/__mock__/log.ts
@@ -1,7 +1,13 @@
 import { vi } from 'vitest';
 
-export default {
+const mockLogger = {
   info: vi.fn(),
   warn: vi.fn(),
   debug: vi.fn(),
+  error: vi.fn(),
+  trace: vi.fn(),
 };
+
+export const createLogger = vi.fn(() => mockLogger);
+
+export default mockLogger;
