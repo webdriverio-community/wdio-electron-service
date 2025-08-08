7a2e7b37a856bff40a82dec04809feb41aa0a0e2
Sam Maister <goosewobbler@protonmail.com>
2025-08-08 11:07:37 +0100

    chore(types): add __markForUpdate

--- packages/@wdio_electron-types/src/index.ts
+++ packages/@wdio_electron-types/src/index.ts
@@ -728,6 +728,12 @@ export interface ElectronMockInstance extends Omit<Mock, Override> {
    * @private
    */
   __isElectronMock: boolean;
+  /**
+   * Used internally to mark the mock as needing an update
+   *
+   * @private
+   */
+  __markForUpdate?: () => void;
 }
 
 export interface ElectronMock<TArgs extends unknown[] = unknown[], TReturns = unknown> extends ElectronMockInstance {
