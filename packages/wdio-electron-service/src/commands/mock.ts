36275486b4a755c22bfc42d4cd615753a85998e6
Sam Maister <goosewobbler@protonmail.com>
2025-08-09 13:11:26 +0100

    chore(service): add mock debug

--- packages/wdio-electron-service/src/commands/mock.ts
+++ packages/wdio-electron-service/src/commands/mock.ts
@@ -1,17 +1,21 @@
 import type { ElectronMock } from '@wdio/electron-types';
+import { createLogger } from '@wdio/electron-utils';
 import { createMock } from '../mock.js';
 import mockStore from '../mockStore.js';
 
+const log = createLogger('mock');
+
 export async function mock(apiName: string, funcName: string): Promise<ElectronMock> {
-  console.log(`[MOCK-DEBUG] mock command called for ${apiName}.${funcName}`);
+  log.debug(`[${apiName}.${funcName}] mock command called`);
   try {
     // retrieve an existing mock from the store
     const existingMock = mockStore.getMock(`electron.${apiName}.${funcName}`);
+    log.debug(`[${apiName}.${funcName}] Found existing mock, resetting`);
     await existingMock.mockReset();
     return existingMock;
   } catch (_e) {
     // mock doesn't exist, create a new one and store it
-    console.log(`[MOCK-DEBUG] Creating new mock for ${apiName}.${funcName}`);
+    log.debug(`[${apiName}.${funcName}] Creating new mock`);
     const newMock = await createMock(apiName, funcName);
     mockStore.setMock(newMock);
     return newMock;
